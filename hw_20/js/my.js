113899c363feda0f0cf1aa0d2d58f73df5982b87
Artemyeva-92 <asartemyeva1@gmail.com>
2022-09-13 16:32:21 +0300

    class corrections hw_20

--- hw_20/js/my.js
+++ hw_20/js/my.js
@@ -2,27 +2,11 @@ let selectColor = document.querySelector('#selectColor');
 let element = document.querySelector('#element');
 let formShape = document.querySelector('#formShape');
 let selectShape = document.querySelector('#selectShape');
-let borderR = 50;
-let squareW = 100;
-
-selectColor.addEventListener('change', event =>{   
-    element.style.backgroundColor = event.target.value;
-})
 
 formShape.addEventListener('submit', event =>{
     event.preventDefault();
-    let shape =  selectShape.value;
-    element.classList.add('square');
-    if(shape === 'rectangle'){
-        element.style.width = (squareW * 2) + `px`;
-        element.style.borderRadius = 0;
-    } else if (shape === 'circle'){
-        element.style.borderRadius = borderR + `%`;
-        element.style.width = squareW + `px`;
-    } else{
-        element.style.borderRadius = 0;
-        element.style.width = squareW + `px`;
-    }
+    element.className = selectShape.value;
+    element.style.backgroundColor = selectColor.value;
 })
 
 
